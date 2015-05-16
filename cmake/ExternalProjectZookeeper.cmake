b2388f9d807d71709ca9eb7686baf4e884654bee
Jason Chaw <jason.chaw@gmail.com>
2015-05-16 00:20:44 -0700

    Include Apache zookeeper patch to handle htonll conflict when compiled on Mac Yosemite. See Apache Zookeeper JIRA ZOOKEEPER-2049

--- cmake/ExternalProjectZookeeper.cmake
+++ cmake/ExternalProjectZookeeper.cmake
@@ -2,12 +2,11 @@ ExternalProject_Add(zookeeper
   PREFIX ${CMAKE_SOURCE_DIR}/deps/zookeeper
   URL http://s3-us-west-2.amazonaws.com/glbin-engine/deps/zookeeper-3.4.6.tar.gz
   URL_MD5 971c379ba65714fd25dc5fe8f14e9ad1
+  PATCH_COMMAND patch -N -p0 -i ${CMAKE_SOURCE_DIR}/patches/zookeeper.patch || true
   PATCH_COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_SOURCE_DIR}/patches/zookeeper/ <SOURCE_DIR>
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND CFLAGS=-fPIC CPPFLAGS=-fPIC CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} ./configure --prefix=<INSTALL_DIR> --disable-shared
   INSTALL_DIR ${CMAKE_SOURCE_DIR}/deps/local)
-
-
 
  macro(requires_zookeeper NAME)
   add_dependencies(${NAME} zookeeper)
